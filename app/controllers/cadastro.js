91ebe06b7ec36c501430c37c45292d201e4f938d
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-09-12 20:35:02 +0000

    Criando a collection jogo e armazenando atributos do jogador

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -22,7 +22,11 @@ module.exports.cadastrar = function(application, req, res){
 	var connection = application.config.dbConnection;
 
 	var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+	var JogoDAO = new application.app.models.JogoDAO(connection);
+
+
 	UsuariosDAO.inserirUsuario(dadosForm);
+	JogoDAO.gerarParametros(dadosForm.usuario);
 
 	var msg = [{msg: "Cadastro realizado com sucesso"}];
 		
