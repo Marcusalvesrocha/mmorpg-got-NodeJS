91bd88cbe5c317ad9b7903c23faa22bb9f24aec3
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-09-13 15:35:30 +0000

    Carregando as views de suditos e pergaminhos na view jogo via ajax

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -11,13 +11,15 @@ module.exports.jogo = function(application, req, res){
 
 	JogoDAO.iniciaJogo(res, req.session.usuario, req.session.casa);
 
-	
-
 }
 
-module.exports.sair = function(application, req, res){
-	req.session.destroy(function(err){
-		var msg = [{msg: "Logof efetuado com sucesso"}];
-		res.render("index", {validacao: {}, mensagem: msg});
-	});
+module.exports.suditos = function(application, req, res){
+	
+		res.render("aldeoes", {validacao: {}, mensagem: {}});
+}
+
+module.exports.pergaminhos = function(application, req, res){
+	
+		res.render("pergaminhos", {validacao: {}, mensagem: {}});
+	
 }
